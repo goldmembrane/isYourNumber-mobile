a92f6b98260e687389bc209224280492afe57185
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-11 21:43:39 +0900

    create title, start button, ranks button box in title page

--- isyournumber_client/src/pages/titlePage.js
+++ isyournumber_client/src/pages/titlePage.js
@@ -1,15 +1,26 @@
 // 앱을 실행할 때, 제일 먼저 화면에 표시될 page
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity ,StyleSheet } from 'react-native'
 
 // title page의 style을 구성할 코드
 
 // title page의 화면을 구성할 코드
 const TitlePage = ({navigation}) => {
     return (
-        <View>
-            <Text>Title</Text>
-        </View>
+        <>
+            {/* game title box */}
+            <View>
+                <Text>Is Your Number?</Text>
+            </View>
+            {/* game start button box */}
+            <TouchableOpacity>
+                <Text>START</Text>
+            </TouchableOpacity>
+            {/* show game ranks button box */}
+            <TouchableOpacity>
+                <Text>Ranks</Text>
+            </TouchableOpacity>
+        </>
     )
 }
 

a6ded62b3ae9dbbfef8bed97b956f5fd1d3a9968
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-12 18:03:30 +0900

    create main game page and create elements in main game page

---
+++ isyournumber_client/src/pages/mainGamePage.js
@@ -0,0 +1,32 @@
+// main game이 이루어지는 main game page
+import React from 'react'
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
+
+// main game page의 style을 구성할 코드
+
+// main game page의 화면을 구성할 코드
+const MainGamePage = () => {
+    return (
+        <>
+            <View>
+
+            </View>
+            <View>
+                <Text>1 + 1 =</Text>
+            </View>
+            <View>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text></Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    )
+}
+
+export default MainGamePage

87a4366bb02164118b8dd60c754be14adc2e750e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-12 17:38:27 +0900

    create setting page style and connect styles to element in setting page

--- isyournumber_client/src/pages/settingPage.js
+++ isyournumber_client/src/pages/settingPage.js
@@ -3,16 +3,51 @@ import React from 'react'
 import { View, Text, TouchableOpacity,StyleSheet } from 'react-native'
 
 // setting page의 style을 구성할 코드
+const settingPageStyles = StyleSheet.create({
+
+    // random number box style
+    randomNumberBox: {
+        width: 250,
+        height: 80,
+        alignSelf: 'center',
+        backgroundColor: '#38f9d7',
+        marginTop: 150,
+    },
+
+    // random number box text style
+    randomNumberBoxText: {
+        fontSize: 30,
+        textAlign: 'center',
+        lineHeight: 80,
+        fontWeight: 'bold',
+    },
+
+    // (setting version) game start button box style
+    gameStartButtonBox: {
+        width: 200,
+        height: 60,
+        alignSelf: 'center',
+        backgroundColor: '#fa71cd',
+        marginTop: 250,
+    },
+
+    // (setting version) game start button box text style
+    gameStartButtonBoxText: {
+        fontSize: 20,
+        textAlign: 'center',
+        lineHeight: 60,
+    }
+})
 
 // setting page의 화면을 구성할 코드
-const SettingPage = () => {
+const SettingPage = ({ navigation }) => {
     return(
         <>
-            <View>
-                <Text>0</Text>
+            <View style = {settingPageStyles.randomNumberBox}>
+                <Text style = {settingPageStyles.randomNumberBoxText}>0</Text>
             </View>
-            <TouchableOpacity>
-                <Text>Game Start!</Text>
+            <TouchableOpacity style = {settingPageStyles.gameStartButtonBox}>
+                <Text style = {settingPageStyles.gameStartButtonBoxText}>Game Start!</Text>
             </TouchableOpacity>
         </>
     )

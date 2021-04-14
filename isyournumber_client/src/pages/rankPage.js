2d852bf6e7a114a7d85bd4ed1e49f5b0266ea8c0
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-15 08:35:28 +0900

    create rank page and import react, react-native

---
+++ isyournumber_client/src/pages/rankPage.js
@@ -0,0 +1,15 @@
+// 자신이 그동안 했던 score들을 높은 점수 순위대로 화면에 표시하는 rank page
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+// rank page의 style을 구성할 코드
+
+// rank page의 화면을 구성할 코드
+const RankPage = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default RankPage

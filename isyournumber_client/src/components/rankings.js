8936c5d21b41a62774d5a39d4a7bd8ae60877e23
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-15 08:51:58 +0900

    create rankings component and connect to rank page, then create styles each rankings component, rank page and connects styles to elements

---
+++ isyournumber_client/src/components/rankings.js
@@ -0,0 +1,30 @@
+// 각 ranking들을 나타낼 rankings component
+
+import React from 'react'
+import { View,  StyleSheet } from 'react-native'
+
+// ranking component의 style을 구성할 코드
+const rankingsStyles = StyleSheet.create({
+
+    // rankings box style
+    rankingsBox: {
+        width: 300,
+        height: 100,
+        backgroundColor: '#2b5876',
+        alignSelf: 'center',
+        marginTop: 30,
+    },
+})
+
+// ranking component의 화면을 구성할 코드
+const Rankings = () => {
+    return(
+        <>
+            <View style = {rankingsStyles.rankingsBox}>
+
+            </View>
+        </>
+    )
+}
+
+export default Rankings
--- isyournumber_client/src/pages/rankPage.js
+++ isyournumber_client/src/pages/rankPage.js
@@ -1,13 +1,28 @@
 // 자신이 그동안 했던 score들을 높은 점수 순위대로 화면에 표시하는 rank page
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import Rankings from '../components/rankings'
 
 // rank page의 style을 구성할 코드
+const rankPageStyles = StyleSheet.create({
+
+    // rank page box style
+    rankPageBox: {
+        width: 350,
+        height: 600,
+        backgroundColor: '#50c9c3',
+        alignSelf: 'center',
+        marginTop: 100,
+    }
+})
 
 // rank page의 화면을 구성할 코드
 const RankPage = () => {
     return (
         <>
+            <View style = {rankPageStyles.rankPageBox}>
+                <Rankings />
+            </View>
         </>
     )
 }

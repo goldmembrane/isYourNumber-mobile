d6e04b3f4b32b0fa048e88c6d5e189ea9488354d
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-04-16 22:56:25 +0900

    create port, cors and server bases at App.js and connect rank router for contrl rank post and get method

---
+++ isyournumber_server/App.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 7500
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const RankRouter = require('./routers/rank')
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+app.use(
+    cors({
+        origin: ['http://localhost:3000'],
+        methods: ['POST', 'GET'],
+        credentials: true,
+    })
+)
+
+app.get('/', (req, res) => {
+    res.status(200).send('Success')
+})
+
+app.use('/rank', RankRouter)
+
+app.set('port', port)
+
+app.listen(app.get('port'), () => {
+    console.log(`app is listening in PORT ${app.get('port')}`)
+})
+
+module.exports = app

658acc24a0d917d75cdc28bacae76452b3d7160e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-05-02 23:41:02 +0900

    fix config.js file about showing password

--- isyournumber_server/config/config.js
+++ isyournumber_server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: 'selectnumber',
-        password: process.env.DATABASE_PASSWORD,
+        password: 'select3593**',
         database: 'isyournumber',
         host: 'isyournumber.csmeh7i5hzpa.ap-northeast-2.rds.amazonaws.com',
         dialect: 'mysql',
